4c653ea9a5cee7f872fbdd71076cdb36462578d3
Thomasin Rawle <thomasin.rawle@skybettingandgaming.com>
2024-06-18 15:46:53 +0100

    adds objects file and tests

--- array.js
+++ array.js
@@ -1,79 +1,53 @@
- // Update the function to add 'element' to the start of 'arr'
-export const pushElementOnToArray = (arr, x) => {
+// Fill in the body of the functions following the instructions.
+// Important! Don't change the name of the functions.
 
-};
+// Update the function to add 'element' to the start of 'arr'
+export const pushElementOnToArray = (arr, x) => {};
 
 // Update the function to remove the last element from 'arr'
-export const removeLastElementFromArray = (arr) => {
-
-};
+export const removeLastElementFromArray = (arr) => {};
 
 // Update the function to remove the first element from 'arr'
-export const removeFirstElementFromArray = (arr) => {
-
-}
+export const removeFirstElementFromArray = (arr) => {};
 
 // Update function to return combined arrays
-export const mergeTwoArraysTogether = (arr1, arr2) => {
-
-}
+export const mergeTwoArraysTogether = (arr1, arr2) => {};
 
 // Update function to return length of array
-export const getArrayLength = (arr) => {
-
-}
+export const getArrayLength = (arr) => {};
 
 // Update function to get the first index of a particular string
-export const getIndexOfStringInArray = (arr, str) => {
-
-}
+export const getIndexOfStringInArray = (arr, str) => {};
 
 // Update function to sort arrays from high to low
-export const sortArrayOfNumbersFromHighToLow = (arr) => {
-
-}
+export const sortArrayOfNumbersFromHighToLow = (arr) => {};
 
 // Update function to return array of numbers each doubled
-export const returnArrayOfNumbersDoubled = (arr) => {
-
-}
+export const returnArrayOfNumbersDoubled = (arr) => {};
 
 // Update function to return array of even numbers
-export const returnArrayOfOnlyEvenNumbers = (arr) => {
-
-}
+export const returnArrayOfOnlyEvenNumbers = (arr) => {};
 
 // Update function to return the total of all numbers in array added together
-export const returnSumOfAllNumbersInArray = (arr) => {
-
-}
+export const returnSumOfAllNumbersInArray = (arr) => {};
 
 // Update function to find the first match
-export const returnTheFirstStringThatMatches = (arr, target) => {
-
-};
+export const returnTheFirstStringThatMatches = (arr, target) => {};
 
 // Update function to return the index of the first match
-export const returnFirstIndexOfString = (arr, target) => {
+export const returnFirstIndexOfString = (arr, target) => {};
 
-};
-
-// Update function to chech that every number is negative
-export const returnsEveryNumberIsNegative = (arr) => {
-
-};
+// Update function to check that every number is negative
+export const returnsEveryNumberIsNegative = (arr) => {};
 
 // Update function to check that at least 1 element is negative
-export const returnsAtLeastOneNumberIsNegative = (arr) => {
-
-};
+export const returnsAtLeastOneNumberIsNegative = (arr) => {};
 
 // Update function to check that array has a particular number/string/whatever
-export const returnsArrayHasElement = (arr, target) => {
-
-};
+export const returnsArrayHasElement = (arr, target) => {};
 
 // Update function to return a single string with attaching strings between
-export const returnsASingleStringWithExtraLettersBetweenEachElement = (arr, extraLetters) => {
-
-};
+export const returnsASingleStringWithExtraLettersBetweenEachElement = (
+  arr,
+  extraLetters
+) => {};
