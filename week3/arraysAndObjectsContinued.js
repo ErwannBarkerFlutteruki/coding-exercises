20536d2874b7e1bd8420348e54fbe657bd6c865a
Heera Rana <heera.rana2@skybettingandgaming.com>
2024-07-11 16:05:05 +0100

    week3-heera

--- week3/arraysAndObjectsContinued.js
+++ week3/arraysAndObjectsContinued.js
@@ -5,6 +5,8 @@ export function getEvenNums(nums) {
   /*
     This function takes an array of integers and returns an array containing only even integers
     */
+	 let evenNums = nums.filter(num => num % 2 === 0)
+	 return evenNums;
 }
 
 export function getWordLengths(str) {
@@ -12,6 +14,12 @@ export function getWordLengths(str) {
     This function takes a string and returns an array of the lengths of each word in the string.
     E.g. 'pineapple and black bean curry' => [9, 3, 5, 4, 5]
     */
+	 if (str.length === 0 ) {
+		return []
+	 }
+	 let words = str.split(" ");
+	 let wordsLength = words.map(word => word.length)
+	 return wordsLength;
 }
 
 export function getMiddleNames(names) {
@@ -20,6 +28,15 @@ export function getMiddleNames(names) {
     If someone does not have a middle name the array should contain the value null at this index.
     E.g. ['Ben Jones', 'Marsha Naomi Jenkins', 'Anna'] => [null, 'Naomi', null]
     */
+	 return names.map(
+		name => {
+			let nameAsArray = name.split(" ")
+			if (nameAsArray.length === 3) {
+				return nameAsArray[1]
+			} 
+			return null;
+		}
+	 ) 
 }
 
 export function getPugNames(dogs) {
@@ -32,10 +49,17 @@ export function getPugNames(dogs) {
     ]
     will return ['Max', 'Poppy']
     */
+	 let pugsOnly = dogs.filter(dog => dog.breed === "Pug");
+	 return pugsOnly.map(pug => pug.name);
 }
 
 export function createUser() {
   // Create an object with properties name set to "Joan" and age set to 70. Then return the object.
+	const newObj = {
+		name: "Joan",
+		age: 70
+	}
+	return newObj;
 }
 
 export function modifyBook(bookObject, genre) {
@@ -44,11 +68,14 @@ export function modifyBook(bookObject, genre) {
   { title: 'Alice`s Adventures in Wonderland', author: 'Lewis Carroll' },
   Add a 'genre' property to this object and set its value to the genre argument passed in to this function. Then return the object.
   */
+ bookObject.genre = genre;
+ return bookObject;
 }
 
 export function copyProduct(product) {
   /* product is an object. Return a shallow copy of product
   N.B You should NOT look at the unit test object and just re-write it */
+	return {...product};
 }
 
 export function checkGardenForSunflowers(garden) {
@@ -63,4 +90,15 @@ export function checkGardenForSunflowers(garden) {
 
   N.B see if there are any Object methods that might help you on MDN
   */
+
+	// Solution 1:
+//  if (garden.sunflowers) {
+// 	return true
+//  } 
+//  return false
+
+// Solution 2
+	const flowerTypes = Object.keys(garden);
+	// ["roses", "tulips", "dahlias", "sunflowers"]
+	return flowerTypes.includes("sunflowers");
 }
--- week3/generalDebugging.js
+++ week3/generalDebugging.js
@@ -1,27 +1,27 @@
 // Do this exercise after ConditionalStatmeents and Operators.
 // in this file the task is to fix the broken code, try and figure out what should happen from the function name and the tests
 
-export const ifXEqualsYRegardlessOfType = (z, y) => {
-    return x = y;
-}
-
-export const ifXEqualsYWithTypeCheck = (x, y) => {
+export const ifXEqualsYRegardlessOfType = (x, y) => {
     return x == y;
 }
 
+export const ifXEqualsYWithTypeCheck = (x, y) => {
+    return x === y;
+}
+
 export const ifXisGreaterThanY = (x, y) => {
-    return x; >= y
+    return x > y;
 }
 
-export const ifXisSmallerThanY = {
-    return x / y;
+export const ifXisSmallerThanY = (x, y) => {
+    return x < y;
 }
 
-export const ifXisGreaterOrEqualToY = (m, y) => {
-    returns z >= y;
+export const ifXisGreaterOrEqualToY = (x, y) => {
+    return x >= y;
 }
 
-export const ifXisSmallerOrEqualToY = (x, y, z) => {
+export const ifXisSmallerOrEqualToY = (x, y) => {
     return x <= y;
 }
 
@@ -29,14 +29,14 @@ export const ifXIsEqualToYOrZWithTypeCheck = (x, y, z) => {
     return (x === y) || (x === z);
 }
 
-export const ifXIsEqualToYAndZWithTypeCheck = (y, x, z) => {
-    (x === y) && (x === z);
+export const ifXIsEqualToYAndZWithTypeCheck = (x, y, z) => {
+    return (x === y) && (x === z);
 }
 
 
-export const ifXisFalseDevideYBy7AndAlwaysReturnY = (x, y) => {
-    if (x == true) {
-        return y / 7;
+export const ifXisTrueDevideYBy7AndAlwaysReturnY = (x, y) => {
+    if (x === true) {
+        return y * 7;
     }
     return y;
 }
