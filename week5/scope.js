59d7b0d8bc7ca9ee8c2bb91a8f1c712144ef131f
erwann barker <erwann.barker@flutteruki.com>
2024-07-22 10:59:39 +0100

    week 5

---
+++ week5/scope.js
@@ -0,0 +1,32 @@
+// create a global variable (to this file) names 'message', return this variable in the below function
+export function returnGlobalMessage() {
+}
+
+// return a message, using a new const variable called 'message'. the Message should equal 'Bye', this should also not break previous tests.
+export function returnLocalMessage() {
+}
+
+//return a passed variable which has to be called 'message' so that what ever is passed to this funciton is returned.
+export function returnPassedMessage() {
+}
+
+//fix this function and explain why it isn't working
+export function fixIfMessageReturn() {
+    if (true) {
+        let message = 'Hey'
+    }
+    message += ', Block!';
+
+    return message;
+}
+
+//fix this function and explain why it isn't working
+export function doubleFilterEvensThenEachNumber(arr) {
+    const events = arr.filter((x) => {
+        const multipliyer = 2;
+        return x % 2 === 0;
+    });
+
+    return evens.map((x) => x * multipliyer);
+}
+
